b4846ceeb88d8102b2e34f5e0a651b209b52c2e3
Olivia Toolson <livytoolson@gmail.com>
2020-12-02 11:42:08 -0800

    initial get request working

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
-// const postsRouter = require('./posts/postRouter.js');
-// const userRouter = require('./users/userRouter.js');
 const server = express();
 
-// const User = require('./users/userDb');
+const userRouter = require('./users/userRouter.js');
+
+server.use(express.json());
+server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 // Custom middleware
-
 function logger(req, res, next) {
   console.log(`Method: ${req.method}, URL: ${req.url}, Timestamp: ${req.timestamp}`)
   next();
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,17 +1,27 @@
 const express = require('express');
 
+const User = require('./userDb');
+const middlewares = require('../api/middlewares');
+
 const router = express.Router();
 
 router.post('/', (req, res) => {
   // do your magic!
+  User.insert()
 });
 
 router.post('/:id/posts', (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // do your magic!
+  try {
+    const users = await User.get(req.query)
+    res.json(users)
+  } catch (error) {
+    res.status(500).json({ message: 'The user information could not be retireved.' })
+  }
 });
 
 router.get('/:id', (req, res) => {
